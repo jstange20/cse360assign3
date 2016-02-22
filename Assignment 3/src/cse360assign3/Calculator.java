c11b54bd728605ca78ad4b0454c5baa4b69b00bf
Jeremy St. Ange <jstange20@gmail.com>
2016-02-22 12:22:34 -0700

    First commit

---
+++ Assignment 3/src/cse360assign3/Calculator.java
@@ -0,0 +1,65 @@
+package cse360assign3;
+
+/**
+ * Simple calculator functions
+ * @author Jeremy St. Ange
+ * @PIN 815
+ * @version Feb 22, 2016
+ */
+public class Calculator {
+
+	private int total;
+	
+	/** Calculator constructor */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** Get the total
+	 * 
+	 * @return total	The integer being worked on	
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** Add value to the total
+	 * 
+	 * @param value		integer added to total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/** Subtract value from the total
+	 * 
+	 * @param value		integer to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/** Multiply total by value
+	 * 
+	 * @param value		integer to be multiplied
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/** Divide the total by value
+	 * 
+	 * @param value		integer to be divided by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** Displays the history
+	 * 
+	 * @return			The history of integers
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
